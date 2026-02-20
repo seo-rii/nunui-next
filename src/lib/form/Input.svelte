ca18deb20509ad50d580f8f50f692b8294e12dee
seorii <me@seorii.page>
2026-02-20 16:11:11 +0000

    chore: remove console.log

--- src/lib/form/Input.svelte
+++ src/lib/form/Input.svelte
@@ -77,7 +77,6 @@
 					}
 					if (_onkeyup) _onkeyup(e);
 					if (type === 'number') {
-						console.log(value);
 						if (typeof value === 'string' && value !== '' && !isNaN(+value)) value = +value;
 					}
 				}
